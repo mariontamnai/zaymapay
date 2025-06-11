86428b5d748e55c01a8501413662fe7a5e7208df
Marion <mariontamnai@gmail.com>
2025-06-11 16:21:03 +0300

    added new lines for new screen

---
+++ app/(tabs)/HomeScreen.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+export default function HomeScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Welcome to ZaymaPay 🤑</Text>
+      <Text>Your balance is Ksh 0.00</Text>
+      {/* Add more features here later! */}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+});
