909fdf5fc6fac21f9260b2a66a050f2a28fbe77d
Marion <mariontamnai@gmail.com>
2025-06-16 14:42:12 +0300

    added new lines

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -7,27 +7,27 @@ function RootLayoutInner() {
   const { user } = useZayma();
   const router = useRouter();
 
-  const [loading, setLoading] = useState(true);
-  useEffect(() => {
-    const timeout = setTimeout(() => {
-      if (!user) {
-        router.replace('/AuthScreen'); // go to Auth
-      } else {
-        router.replace('/(tabs)/HomeScreen'); // go to home
-      }
-      setLoading(false);
-    }, 1000);
+  //const [loading, setLoading] = useState(true);
+  //useEffect(() => {
+    //const timeout = setTimeout(() => {
+      //if (!user) {
+        //router.replace('/AuthScreen'); // go to Auth
+      //} else {
+        //router.replace('/(tabs)/HomeScreen'); // go to home
+      //}
+      //setLoading(false);
+    //}, 1000);
 
-    return () => clearTimeout(timeout);
-  }, [user]);
+    //return () => clearTimeout(timeout);
+  //}, [user]);
 
-  if (loading) {
-    return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <ActivityIndicator size="large" color="#27ae60" />
-      </View>
-    );
-  }
+  //if (loading) {
+    //return (
+      //<View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        //<ActivityIndicator size="large" color="#27ae60" />
+     //</View>
+   // );
+  //}
 
   return <Slot />;
 }
