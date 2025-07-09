2620be32bbb0a4c37048e706f18762175b96b32b
Marion <mariontamnai@gmail.com>
2025-07-09 16:22:50 +0300

    updated the app and asset files

---
+++ assets/index.ts
@@ -0,0 +1,6 @@
+export default {
+    signupBg: require('./young-woman-doing-shopping-online.jpg'),
+    homeBg: require('./freepik__the-style-is-candid-image-photography-with-natural__82401.png'),
+    // Add more images here as you go
+  };
+  
